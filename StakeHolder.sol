e29d3e3be427497b065ae4ee27a241bf9f18b5a0
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-10-30 11:39:00 +0100

    whitelist

--- StakeHolder.sol
+++ StakeHolder.sol
@@ -2,7 +2,10 @@
 pragma solidity  >=0.6.6;
 
 contract StakeHolder {
+    address public immutable contractOwner;
+    address [] whiteList;
+    
     constructor() {
-        
-    }
+        contractOwner = msg.sender;
+    };
 }
