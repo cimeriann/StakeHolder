c6915a553caa586e1cc0742e560db1919c1e4d6a
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-10-30 12:05:57 +0100

    create functions to check staking status

--- StakeHolder.sol
+++ StakeHolder.sol
@@ -3,9 +3,35 @@ pragma solidity  >=0.6.6;
 
 contract StakeHolder {
     address public immutable contractOwner;
-    address [] whiteList;
-    
+    uint256 constant public StakingPeriod = 365;
+    // keep track of the number of days that have passed since staking began
+    uint256 public numberOfDaysSinceStakingBegan;
+    // keep track of funders
+    struct Funder {
+        address fundersAddress;
+        uint258 amountFunded;
+    }
+    Funder [] public funders;
+    enum StakingStatus { WaitingToStake, StakingHasBegun, StakingHasEnded};
+    StakingStatus public status;
     constructor() {
         contractOwner = msg.sender;
+        status = StakingStatus.WaitingToStake;
     };
+
+    function activate() public {
+        status = StakingStatus.StakingHasBegun;
+    };
+
+    function isActive() public returns(bool){
+        return status == Status.StakingHasBegun;
+    }
+    function stakingEnded() public returns(bool){
+        if (numberOfDaysSinceStakingBegan == StakingPeriod) {
+            status = Status.StakingHasEnded;
+            return status == Status.StakingHasEnded;
+        } else {
+            return false;
+        }
+    }
 }
