69277c05e12434f551ba94142b954ceb5a5eaaae
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-11-01 13:08:04 +0100

    add beforeEach to tests

--- StakeHolder-Contract/contracts/StakeHolder.sol
+++ StakeHolder-Contract/contracts/StakeHolder.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  >=0.8.0;
+pragma solidity >=0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
@@ -10,76 +10,96 @@ contract StakeHolder {
     using PriceConverter for uint256;
 
     address public immutable contractOwner;
-    uint256 constant public StakingPeriod = 365;
+    uint256 public constant StakingPeriod = 365;
     // keep track of the number of days that have passed since staking began
     uint256 public numberOfDaysSinceStakingBegan;
-    uint256 public constant MINIMUM_USD = 100 * 10 ** 18;
+    uint256 public constant MINIMUM_USD = 100 * 10**18;
     // create new funder
     // struct Funder {
     //     uint256 _id;
     //     address _fundersAddress;
-    //     uint258 _amountFunded;
+    //     uint256 _amountFunded;
     // }
+    // Funder[] public funders;
 
-    mapping (address => uint256) public addressToAmountFunded;
+    mapping(address => uint256) public addressToAmountFunded;
     // mapping(uint => Funder) public funders;
     address[] public funders;
 
     uint256 public funderCount = 0;
 
-    enum StakingStatus { WaitingToStake, StakingHasBegun, StakingHasEnded }
+    // enum StakingStatus { WaitingToStake, StakingHasBegun, StakingHasEnded }
+    struct StakingStatus {
+        bool WaitingToStake;
+        bool StakingHasBegun;
+        bool StakingHasEnded;
+    }
     StakingStatus public status;
 
     constructor() {
         contractOwner = msg.sender;
+        status.WaitingToStake = true;
     }
 
-    function fund() public payable{
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more avax");
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "You need to spend more avax"
+        );
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
+
+        // addFunder(msg.sender, msg.value);
     }
 
-    // function addFunder(address memory _funderAddress, uint256 memory _amountFunded){
-    //     //incomplete, hold on for now
+    // function addFunder(address _funderAddress, uint256 _amountFunded) internal returns(uint256){
     //     funderCount+=1;
-    //     funders[funderCount] = Funder(funderCount, _funderAddress, _amountFunded);
-
-    // };
-    // function activate() public returns(bool){
-    //     status = StakingStatus.StakingHasBegun;
-    //     return status == StakingStatus.StakingHasBegun;
-    // };
-
-    modifier onlyOwner {
+    //     // funders.push(Funder(funderCount, _funderAddress, _amountFunded));
+    //     return funderCount;
+    // }
+    modifier onlyOwner() {
         require(msg.sender == contractOwner, "UNAUTHORIZED!!");
         if (msg.sender != contractOwner) revert NotOwner();
         _;
     }
 
-    // function isActive() public returns(bool){
-    //     return status == Status.StakingHasBegun;
-    // };
-
-    function stakingEnded() public returns(bool){
-        if (numberOfDaysSinceStakingBegan == StakingPeriod) {
-            status = StakingStatus.StakingHasEnded;
-            return status == StakingStatus.StakingHasEnded;
-        } else {
-            return false;
-        }
+    function activate() public onlyOwner returns (bool) {
+        status.StakingHasBegun = true;
+        return status.StakingHasBegun;
     }
 
-    function withdraw() onlyOwner public{
+    function stakingStarted() public returns (bool) {
+        status.StakingHasBegun = true;
+        return status.StakingHasBegun;
+    }
+
+    // function stakingEnded() public returns(bool){
+    //     function incomplete
+    //     if (numberOfDaysSinceStakingBegan == StakingPeriod) {
+    //         status.StakingHasEnded;
+    //         return status.StakingHasEnded;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+    function withdraw() public onlyOwner {
         //loop through the funders array and set their amount funded to zero
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
 
         funders = new address[](0);
+        // funders = new address[](0);
         //transfer funds to owner
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
 
@@ -87,13 +107,13 @@ contract StakeHolder {
     fallback() external payable {
         fund();
     }
-    
+
     receive() external payable {
         fund();
     }
 
     //function to check contract's balance
-    function checkBalance() public view returns(uint256){
+    function checkBalance() public view returns (uint256) {
         return address(this).balance;
     }
 }
--- StakeHolder-Contract/test/StakeHolder.test.js
+++ StakeHolder-Contract/test/StakeHolder.test.js
@@ -1,42 +1,50 @@
 const { expect } = require("chai");
-const { ethers } = require("ethers");
+const { ethers } = require("hardhat");
 
-describe("StakeHolder Contract",  async () => {
-    it("deployment should set status to WaitingToStake", async () => {
-        const [owner] = await ethers.getSigners();
-
+describe("StakeHolder Contract", async () => {
+    let deployer;
+    let stakeHolder;
+    beforeEach(async () => {
+        deployer = ethers.getSigners();
         const StakeHolder = await ethers.getContractFactory("StakeHolder");
-        const stakeHolder = await StakeHolder.deploy();
-        await stakeHolder.deployed();
-        //test incomplete, hold on for now
-    });
-    it("the fund function should send avax to the smart contract", async function() {
-        //hold on for now
-
+        stakeHolder = await(StakeHolder.deploy());
+        console.log("contract deployed at:", stakeHolder.address);
 
     });
-
-    it("the withdraw function should withdraw funds from the smartcontract", async function() {
-        //should only work if the "caller" of withdraw is the contractOwner
-        //hold on for now
-
-    });
-
-    it("the addFunder function should add a funder to the funders' list", async function(){
-        //hold on for now
-    });
-
-    it("activate function should return true if staking has begun, else false", async function(){
-        //hold on for now
-    });
-
-
-})
-
-describe("Price Converter smart contract", async function(){
-    it("getPrice should return the price of avax in dollars", async function(){
-    const [signer] = ethers.getSigners();
-
+    afterEach(async () => {
+        // cleanup
+        deployer = null;
+        stakeHolder = null;
     })
+  it("deployment should set status to WaitingToStake", async () => {
+    const stakeHolder = await StakeHolder.deploy();
+    await stakeHolder.deployed();
 
+    const waitingToStakeStat = true;
+    expect(await stakeHolder.status.waitingToStake).to.equal(
+      waitingToStakeStat
+    );
+  });
+  it("the fund function should send avax to the smart contract", async () => {
+    //hold on for now
+  });
+
+  it("the withdraw function should withdraw funds from the smartcontract", async () => {
+    //should only work if the "caller" of withdraw is the contractOwner
+    //hold on for now
+  });
+
+  it("the addFunder function should add a funder to the funders' list", async () => {
+    //hold on for now
+  });
+
+  it("activate function should return true if staking has begun, else false", async () => {
+    //hold on for now
+  });
+});
+
+describe("Price Converter smart contract", async function () {
+  it("getPrice should return the price of avax in dollars", async function () {
+    const [signer] = ethers.getSigners();
+  });
 });
