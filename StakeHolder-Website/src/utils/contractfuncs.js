89bce0c5afdcd09269984edca38a02568a788698
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-11-29 22:05:43 +0100

    comment out unused imports

--- StakeHolder-Website/src/utils/contractfuncs.js
+++ StakeHolder-Website/src/utils/contractfuncs.js
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import BN from "bn.js";
+// import BN from "bn.js";
 import { abi } from "../constants/abi";
 import { stakeHolderAddress } from "../constants/constants";
 
