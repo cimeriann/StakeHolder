16dba6af8f249eebfa36e97d6cbcd3a9b5622c12
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-11-30 22:11:43 +0100

    implement fund func

--- StakeHolder-Website/src/components/Form.jsx
+++ StakeHolder-Website/src/components/Form.jsx
@@ -1,24 +1,64 @@
 import React, { useState } from "react";
-import { Fund } from "../utils/contractfuncs";
+import { Fund, Withdraw } from "../utils/contractfuncs";
 import Button from "./UI/Button";
 import Input from "./UI/Input";
 export const Form = (props) => {
   const [fundAmount, setFundAmount] = useState("");
-  const handleChange = (e) => {
-    setFundAmount(e.target.value);
+  // useEffect(() => {
+  //   setFundAmount(_fundAmount);
+  // });
+  // let _fundAmount;
+  // const handleChange = ;
+  const resetFundAmount = () => {
+    setFundAmount("");
   };
-  if (props.isWalletConnected) {
-    return (
-      <div>
-        <Input
-          value={fundAmount}
-          onChange={handleChange}
-          description="Fund Amount:"
-        />
-        <Button onClick={Fund(fundAmount)}>Fund Me</Button>
-      </div>
-    );
-  } else {
-    return <div>{props.status}</div>;
-  }
+  const onWithdrawClicked = async (e) => {
+    try {
+      await Withdraw();
+    } catch (err) {
+      alert(err);
+    }
+  };
+  const onFundClicked = async (e) => {
+    e.preventDefault();
+
+    const txHash = await Fund(fundAmount);
+    console.log(txHash);
+    if (txHash.success) {
+      resetFundAmount();
+      console.log("success");
+    }
+  };
+
+  return (
+    <div>
+      <Input
+        value={fundAmount}
+        onChange={(e) => {
+          setFundAmount(e.target.value);
+        }}
+        description="Fund Amount:"
+      />
+      <Button
+        onClick={async (e) => {
+          if (props.isWalletConnected) {
+            onFundClicked(e);
+          } else {
+            alert("You have to connect your wallet before funding!");
+          }
+        }}
+      >
+        Fund Me
+      </Button>
+      <Button
+        onClick={async (e) => {
+          if (props.isWalletConnected) {
+            onWithdrawClicked(e);
+          } else {
+            alert("You have to connect your wallet");
+          }
+        }}
+      ></Button>
+    </div>
+  );
 };
--- StakeHolder-Website/src/components/UI/Input.jsx
+++ StakeHolder-Website/src/components/UI/Input.jsx
@@ -7,7 +7,14 @@ const Input = (props) => {
       <label htmlFor={id} className="ui-input-desc">
         {props.description}
       </label>
-      <input id={id} className="ui-input-field" type="text" placeholder="10" />
+      <input
+        id={id}
+        className="ui-input-field"
+        value={props.value}
+        onChange={props.onChange}
+        type="text"
+        placeholder="10"
+      />
     </div>
   );
 };
